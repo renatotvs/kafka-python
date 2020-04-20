e708b9ba05e718fbfb9bc53b675bb7a5a417b454
Renato Tavares da Silva <r.tavares.da.silva@avanade.com>
2020-04-19 21:29:32 -0300

    Kafka com python

--- get_message_kafka.py
+++ get_message_kafka.py
@@ -3,13 +3,13 @@
 from kafka import KafkaConsumer
 import json
 
-
+# Cria uma instancia com o Kafka consumer
 consumer = KafkaConsumer('kafka-python-topic', bootstrap_servers=['localhost:9092'],
                          auto_offset_reset='earliest',
                          enable_auto_commit=True, group_id='my-group',
                          value_deserializer=lambda x: json.loads(x.decode('utf-8')))
 
-
+# busca mensagens na fila
 for message in consumer:
     print(message.value)
 
--- send_message_kafka.py
+++ send_message_kafka.py
@@ -9,13 +9,15 @@ import os
 
 DEFAULT_UUID = uuid.UUID(bytes=os.urandom(16), version=4)
 
-# Cria uma instÃ¢ncia com o Kafka producer
+# Cria uma instancia com o Kafka producer
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda x: json.dumps(x).encode('utf-8'))
 
+# formato json
 jsonMessageToSend = {'uuid': str(DEFAULT_UUID),
                      'company_id': 1,
                      'country': 'Brazil'}
 
+# producer.send - método que grava na fila
 producer.send('kafka-python-topic', value=jsonMessageToSend)
 
